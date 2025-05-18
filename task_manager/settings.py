80d4d55e2808df64d01a2cd6cbc8e3980a860a00
Taras Shevchyk <tarashevchik27@gmail.com>
2025-05-18 23:10:28 +0300

    add static directory

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static", 
+]
+
 STATIC_URL = "static/"
 
 # Default primary key field type
@@ -132,3 +136,5 @@ INTERNAL_IPS = [
 ]
 
 AUTH_USER_MODEL = "manager.Worker"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
