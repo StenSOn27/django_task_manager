d4441f5848b110416f2d1e03e20327d21726a7be
Taras Shevchyk <tarashevchik27@gmail.com>
2025-05-18 16:02:03 +0300

    implement Task, Worker, Position models

---
+++ manager/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position, on_delete=models.SET_NULL, null=True, related_name="workers"
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.position})"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        URGENT = "Urgent", "Urgent"
+        HIGH = "High", "High"
+        MEDIUM = "Medium", "Medium"
+        LOW = "Low", "Low"
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10, choices=Priority.choices, default=Priority.MEDIUM
+    )
+    task_type = models.ForeignKey(
+        TaskType, on_delete=models.CASCADE, related_name="tasks"
+    )
+    assignees = models.ManyToManyField("Worker", related_name="tasks", blank=True)
+
+    def __str__(self):
+        return self.name
